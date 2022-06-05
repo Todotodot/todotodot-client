bcf01452feb88a9ad8e416f2690b3ee7e694b450
daeunKim <73865700+KimDaEun1031@users.noreply.github.com>
2022-06-05 20:00:18 +0900

    로그인 구현 완료했습니다. (#3)
    * chore: Initial Setup
    
    * chore: Setup Packages
    
    * feat: Login & Save localstorage JWT token
    
    * refactor: Api/index.js
    
    Co-authored-by: daeunKim

---
+++ src/api/index.js
@@ -0,0 +1,46 @@
+import Axios from "axios";
+
+const API = Axios.create({ baseURL: "http://localhost:5000" });
+
+export const login = async (firebaseToken) => {
+  try {
+    await API.post(
+      "/login",
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${firebaseToken}`,
+        },
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).jwtToken
+    }`;
+  }
+
+  return req;
+});
+
+API.interceptors.response.use(
+  (res) => {
+    if (res.data.token) {
+      localStorage.setItem("profile", res.data.token);
+    }
+
+    return res;
+  },
+  (err) => {
+    if (err.response.status === 401) {
+      location.href = "/";
+    }
+
+    return Promise.reject(err);
+  }
+);
--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import Login from "../components/Login/Login";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
