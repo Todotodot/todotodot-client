d53795c1728bedd066d9ae0a14731804add87d2e
KimDaEun1031 <kimde1031@gmail.com>
2022-06-06 03:30:18 +0900

    design: Login view & add button component

---
+++ src/components/shared/Button.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: 300px;
+  height: 50px;
+  margin-top: 100px;
+  outline: none;
+  border: 3px solid #49251c;
+  border-radius: 10px;
+  background-color: #ec7665;
+  color: white;
+  font-size: 18px;
+  cursor: pointer;
+`;
+
+export default Button;
