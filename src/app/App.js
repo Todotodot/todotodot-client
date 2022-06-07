465c9afe4b6d94ebe0e18242f8f836d26e62c5ec
eggfreitag <93723399+eggfreitag@users.noreply.github.com>
2022-06-07 15:37:07 +0900

    chore: Set up temporary router (#6)
    Co-authored-by: Wonhee Kang <eggfreitag@MacMorning.local>

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Login from "../components/Login/Login";
+import Profile from "../components/Profile/Profile";
+import InGame from "../components/Game/InGame";
+import Loading from "../components/Game/Loading";
+import GroupList from "../components/GroupList/GroupList";
+import Modal from "../components/Modal/Modal";
+import TodoList from "../components/TodoList/TodoList";
+import UserList from "../components/UserList/UserList";
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/modal" element={<Modal />} />
+        <Route path="/inGame" element={<InGame />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/loading" element={<Loading />} />
+        <Route path="/todoList" element={<TodoList />} />
+        <Route path="/userList" element={<UserList />} />
+        <Route path="/groupList" element={<GroupList />} />
+      </Routes>
+    </Router>
   );
 }
 
