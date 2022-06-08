63444791f0b430054ed34cb0321395db05f5b559
KimDaEun1031 <kimde1031@gmail.com>
2022-06-08 18:23:10 +0900

    feat: TodoList view and GroupList view

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,7 +1,82 @@
 import React from "react";
+import styled from "styled-components";
 
-function TodoList() {
-  return <div>TodoList</div>;
-}
+import Line from "../shared/Line";
+import Button from "../shared/Button";
+import ListItemContainer from "../shared/ListItemContainer";
+import MaincContainer from "../shared/MainContainer";
+
+import pencil from "../../assets/images/icons/pencil.png";
+import trashCan from "../../assets/images/icons/trash-can.png";
+
+const TodoList = () => {
+  return (
+    <MaincContainer>
+      <TodoListHeader>
+        <div className="listTitle">
+          <p className="title">Todo</p>
+          <Line />
+        </div>
+        <TodoListDropDown>
+          <option value="inCompleted">inCompleted</option>
+          <option value="Completed">Completed</option>
+          <option value="All">All</option>
+        </TodoListDropDown>
+      </TodoListHeader>
+      <ul className="listBody">
+        <ListItemContainer>
+          <label>
+            <input type="checkbox" className="todoCompleteBtn" name="completed" />
+            <span className="checkbox"></span>
+          </label>
+          <p className="title">todo title</p>
+          <div className="btnGroup">
+            <button className="updateBtn">
+              <img src={pencil} alt="pencil" />
+            </button>
+            <button className="deleteBtn">
+              <img src={trashCan} alt="trashCan" />
+            </button>
+          </div>
+        </ListItemContainer>
+      </ul>
+      <TodoCreateBtn>Create</TodoCreateBtn>
+    </MaincContainer>
+  );
+};
+
+const TodoListHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+
+  .title {
+    margin-left: 5px;
+    font-size: 26px;
+    font-weight: bold;
+    color: #ec7665;
+  }
+`;
+
+const TodoListDropDown = styled.select`
+  width: 160px;
+  padding: 3px 5px;
+  border: 2px solid #49251c;
+  outline: none;
+  border-radius: 5px;
+  background-color: #ECA2A2;
+  font-size: 18px;
+  color: white;
+  cursor: pointer;
+`;
+
+const TodoCreateBtn = styled(Button)`
+  float: right;
+  width: 120px;
+  height: 35px;
+  margin: 0 20px;
+  padding: 2px 0;
+`;
 
 export default TodoList;
---
+++ src/components/shared/Line.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Line = styled.div`
+  width: 300%;
+  height: 3px;
+  border-radius: 5px;
+  background-color: rgba(73, 37, 28, 0.6);
+`;
+export default Line;
