d913ac7b8c99e0b86a8b84ae1986e7b714cced9d
JihyunGong <jihyun2462@gmail.com>
2022-06-17 15:51:27 +0900

    fix: Fix bug for deployment

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,12 +1,8 @@
-import { useNavigate } from "react-router-dom";
-
-const navigate = useNavigate();
-
 const catchAsync = (asyncFunction) => async (req, res) => {
   try {
     await asyncFunction(req, res);
   } catch (err) {
-    navigate("*");
+    console.error(err);
   }
 };
 
